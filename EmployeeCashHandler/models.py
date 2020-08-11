ba1a2febeef99829cda3ac767e2db65cb3ab031e
igardner23 <igard2014@gmail.com>
2020-08-11 21:38:02 +0000

    origin

--- EmployeeCashHandler/models.py
+++ EmployeeCashHandler/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
+    first_name = models.CharHahahField(max_length=100)
     last_name = models.CharField(max_length=100)
     register_bal = models.FloatField()
-    id = models.Like 
+    history=HistoricalRecords()
 
 class CashDrawerHistory(models.Model):
     start_bal = models.FloatField()
     end_bal = models.FloatField()
     datetime = models.DateTimeField()
     employee = models.ForeignKey(Employee)
+    history=HistoricalRecords()
 
 class Store(models.Model):
     storename = models.CharField(max_length=50)
